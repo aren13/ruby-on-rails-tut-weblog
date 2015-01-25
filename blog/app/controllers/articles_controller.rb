0cbd3d6a588447dc29f5135e92cca401bd2874b2
aren13 <ardaeren13@gmail.com>
2015-01-26 01:20:49 +0200

    adding some validations for article and checking validation errors on new article form.

--- blog/app/controllers/articles_controller.rb
+++ blog/app/controllers/articles_controller.rb
@@ -5,14 +5,19 @@ class ArticlesController < ApplicationController
 	end
 
 	def new
+		@article = Article.new
 	end
 
 	def create
 		#render plain: params[:article].inspect
 		@article = Article.new(article_params)
 
-		@article.save
-		redirect_to @article
+		if @article.save
+    		redirect_to @article
+  		else
+    		render 'new'
+  		end
+  		#Notice that inside the create action we use render instead of redirect_to when save returns false. The render method is used so that the @article object is passed back to the new template when it is rendered. This rendering is done within the same request as the form submission, whereas the redirect_to will tell the browser to issue another request.	
 	end
 
 	def show
--- blog/app/models/article.rb
+++ blog/app/models/article.rb
@@ -1,2 +1,8 @@
 class Article < ActiveRecord::Base
+	validates :title, presence: true,
+                    length: { minimum: 5 }
+
 end
+
+#Validations are covered in detail in Active Record Validations. 
+#(http://guides.rubyonrails.org/active_record_validations.html)
