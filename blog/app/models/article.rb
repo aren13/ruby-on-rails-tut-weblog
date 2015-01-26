8ad9621bb506d8d766dba950e0e461580bec7585
aren13 <ardaeren13@gmail.com>
2015-01-26 15:30:38 +0200

    Deleting Associated Objects.

--- blog/app/models/article.rb
+++ blog/app/models/article.rb
@@ -1,6 +1,6 @@
 class Article < ActiveRecord::Base
 	#Active Record Associations:: http://guides.rubyonrails.org/association_basics.html
-	has_many :comments 
+	has_many :comments, dependent: :destroy
 	validates :title, presence: true,
                     length: { minimum: 5 }
 
