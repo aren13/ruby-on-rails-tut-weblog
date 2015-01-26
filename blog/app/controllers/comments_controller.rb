a858486d6a918a9a4b852f02fc537318383febcf
aren13 <ardaeren13@gmail.com>
2015-01-26 15:29:47 +0200

    Deleting Comments

--- blog/app/controllers/comments_controller.rb
+++ blog/app/controllers/comments_controller.rb
@@ -2,6 +2,15 @@ class CommentsController < ApplicationController
 	def create
 		@article = Article.find(params[:article_id])
 		@comment = @article.comments.create(comment_params)
+
+		redirect_to article_path(@article)
+	end
+
+	def destroy 
+		@article = Article.find(params[:article_id])
+		@comment = @article.comments.find(params[:id])
+		@comment.destroy
+
 		redirect_to article_path(@article)
 	end
 
@@ -9,4 +18,5 @@ class CommentsController < ApplicationController
 		def comment_params
 			params.require(:comment).permit(:commenter, :body)
 		end
+		
 end
